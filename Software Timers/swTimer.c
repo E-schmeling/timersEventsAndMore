070ae87772c9ca5a165cde898e86c2514f2ee3d5
ers <112107257+E-schmeling@users.noreply.github.com>
2026-07-16 20:24:19 -0400

    things were borked up I am aborting the good commit message temporarily

--- Software Timers/swTimer.c
+++ Software Timers/swTimer.c
@@ -22,7 +22,7 @@
  *  Function prototypes for internal routines
  *  =======================================================================
  */
-static uint32_t get_TimeMS(void);
+static uint32_t get_Time(void);
 
 /** =======================================================================
  *  Global Variables
@@ -43,15 +43,7 @@ static uint32_t timerPrev[TIMER_COUNT] = {0};
  *  Device-specific implementations (to be customized)
  *  =======================================================================
  */
-/**
- * @internal
- * @brief Get the current time in milliseconds.
- *
- * @return The current time in milliseconds.
- *
- * @note This function should be implemented according to the specific hardware platform being used.
- */
-static uint32_t get_TimeMS(void)
+static uint32_t get_Time(void)
 {
 
 
@@ -62,44 +54,31 @@ static uint32_t get_TimeMS(void)
  *  Public API
  *  =======================================================================
  */
-uint8_t timerSetDelay(TimerID whichTimer, uint32_t delayMs)
+
+// Set the delay for a timer at runtime
+void timerSetDelay(TimerID whichTimer, uint32_t delayMs)
 {
     if (whichTimer < TIMER_COUNT)
     {
         timerDelay[whichTimer] = delayMs;
     }
-    else
-    {
-        return 1; // Error: Invalid Timer ID
-    }
 }
 
-uint8_t timerReset(TimerID whichTimer)
+void timerReset(TimerID whichTimer)
 {
     if (whichTimer < TIMER_COUNT)
     {
         timerPrev[whichTimer] = get_Time();
     }
-    else
-    {
-        return 1; //Error: Invalid Timer ID
-    }
 }
 
-uint8_t timerAwait(TimerID whichTimer)
+bool awaitTimer(TimerID whichTimer)
 {
-    uint32_t now = get_TimeMS();
-    if (whichTimer >= TIMER_COUNT)
-    {
-        return 2; //Error: Invalid Timer ID
-    }
-    
-    
+    uint32_t now = get_Time();
     if ((uint32_t)(now - timerPrev[whichTimer]) >= timerDelay[whichTimer])
     {
         timerPrev[whichTimer] = now;
-        return 0;
+        return true;
     }
-
-    return 1;
+    return false;
 }
--- Software Timers/swTimer.h
+++ Software Timers/swTimer.h
@@ -13,9 +13,10 @@
  *  =======================================================================
  */
 #include <stdint.h>
+#include <stdbool.h>
 
 /** =======================================================================
- *  Enumerations and Types
+ *  
  *  =======================================================================
  */
 typedef enum
@@ -29,47 +30,11 @@ typedef enum
 } TimerID;
 
 
-/** =======================================================================
- *  Public API 
- *  =======================================================================
- */
 
+bool awaitTimer(TimerID whichTimer);
 
-/**
- * @brief Reset the timer.
- * 
- * @param whichTimer The id of the timer to reset.
- * 
- * @return return errors:
- *              0 - Success
- *              1 - Error: Invalid Timer ID
- * 
- * @note When starting a timer it must be reset or else it will immediately reset
- */
-uint8_t timerReset(TimerID whichTimer);
+void timerReset(TimerID whichTimer);
 
-/**
- * @brief Set the delay for a timer.
- * 
- * @param whichTimer The id of the timer to set the delay for.
- * @param delayMs The new delay in milliseconds.
- * 
- * @return return errors:
- *              0 - Success
- *              1 - Error: Invalid Timer ID
- */
-uint8_t timerSetDelay(TimerID whichTimer, uint32_t delayMs);
-
-/**
- * @brief Check if the timer has expired.
- * 
- * @param whichTimer The id of the timer to reset.
- * 
- * @return The timer status of the timer. 
- *              0 - Timer has expired
- *              1 - Timer has not expired
- *              2 - Error: Invalid Timer ID
- */
- uint8_t timerAwait(TimerID whichTimer);
+void timerSetDelay(TimerID whichTimer, uint32_t delayMs);
 
 #endif // TIMER_H
